5fe587d636d6911e986dcaf98e52d4e10c289c29
marucircle <2000ryo0113@ezweb.ne.jp>
2023-01-08 01:16:47 +0900

    feat: blog取得のAPI仮作成

--- src/api/blog/index.ts
+++ src/api/blog/index.ts
@@ -1,3 +1,27 @@
 import CMSClient from '../client'
 
-export const getOneBlog = () => {}
+export const getOneBlog = async ({ id }: { id: string }) => {
+  return await CMSClient.getByUID('blogs', id)
+}
+
+export const getManyBlog = async ({ page }: { page: number }) => {
+  return await CMSClient.getByType('blogs', {
+    orderings: {
+      //TODO 並び替えについて検討
+      field: 'document.last_publication_date',
+      direction: 'desc',
+    },
+    page: page,
+    pageSize: 20, //TODO サイズは検討
+  })
+}
+
+export const getAllBlog = async () => {
+  return await CMSClient.getAllByType('blogs', {
+    orderings: {
+      //TODO 並び替えについて検討
+      field: 'document.last_publication_date',
+      direction: 'desc',
+    },
+  })
+}
