735e0beaa463bc3591901bd3db2bff2965dd85d2
marucircle <2000ryo0113@ezweb.ne.jp>
2023-01-10 05:22:04 +0900

    feat: slicezone実装

---
+++ src/components/vue/prismic/RichEditor.vue
@@ -0,0 +1,18 @@
+<template>
+  <PrismicRichText :field="slice.primary.rich_editor" />
+</template>
+
+<script>
+import { PrismicRichText } from '@prismicio/vue'
+
+export default {
+  name: 'RichEditor',
+  components: { PrismicRichText },
+  props: {
+    slice: Object,
+  },
+  created() {
+    console.log(this.slice.primary)
+  },
+}
+</script>
---
+++ src/components/vue/prismic/SliceZone.vue
@@ -0,0 +1,22 @@
+<template>
+  <SliceZone :slices="slices" :components="{ rich_editor: RichEditor }" />
+</template>
+
+<script>
+import { SliceZone } from '@prismicio/vue'
+import RichEditor from '@/components/vue/prismic/RichEditor.vue'
+
+export default {
+  data() {
+    return {
+      RichEditor,
+    }
+  },
+  components: {
+    SliceZone,
+  },
+  props: {
+    slices: { type: Object, isArray: true, required: true },
+  },
+}
+</script>
