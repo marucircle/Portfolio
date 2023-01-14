8ace43e5147a8c74a94fae622a19e2f3a8d6eafa
marucircle <2000ryo0113@ezweb.ne.jp>
2023-01-15 03:57:10 +0900

    feat: dateFormatでnullの際に異常値を返すようコードを追加

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,6 +1,7 @@
 import { cdate } from 'cdate'
 
-const dateFormat = function (date: string) {
+const dateFormat = function (date: string | null) {
+  if (date === null) return 'XXXX/XX/XX'
   return cdate(date).locale('ja').format('YYYY/MM/DD')
 }
 
