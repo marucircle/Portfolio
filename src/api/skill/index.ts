ce66fcadaba236329359092cf24ec1c52ac6b220
marucircle <2000ryo0113@ezweb.ne.jp>
2023-01-15 21:29:46 +0900

    feat: skill一覧取得の関数作成

---
+++ src/api/skill/index.ts
@@ -0,0 +1,14 @@
+import * as prismic from '@prismicio/client'
+import CMSClient from '../client'
+
+export const getSkillList = async (tags: Array<string>) => {
+  return await CMSClient.getAllByType('skill', {
+    orderings: {
+      field: 'document.level',
+      direction: 'desc',
+    },
+    predicates: [
+      prismic.predicate.at('document.tags', tags), //タグの絞り込み
+    ],
+  })
+}
